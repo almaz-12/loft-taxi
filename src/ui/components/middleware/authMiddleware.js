8b2b53dc6dde098c0e5e5756216eb45948e4cba7
almaz-12 <adreams2016@yandex.ru>
2021-05-17 09:46:50 +0300

    Update authMiddleware.js

--- src/ui/components/middleware/authMiddleware.js
+++ src/ui/components/middleware/authMiddleware.js
@@ -1,12 +1,14 @@
 import { AUTHENTICATE, logIn } from "../actions";
-import apiLogIn from '../api'
+import apiLogIn from '../api';
+import { Redirect } from "react-router-dom";
 
 export const authMiddleware = (store) => (next) => async (action) => {
   if (action.type === AUTHENTICATE) {
     const {email, password} = action.payload;
     const success = await apiLogIn(email, password)
     if(success){
-      store.dispatch(logIn())
+      store.dispatch(logIn());
+      <Redirect to="/map" />
     }
   } else {
     next(action);
