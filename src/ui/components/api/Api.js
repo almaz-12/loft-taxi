51ef6bbfe73c91e34a203bcecb53b40cc57621dc
almaz-12 <adreams2016@yandex.ru>
2021-05-17 17:15:11 +0300

    Update App.js

--- src/ui/components/api/Api.js
+++ src/ui/components/api/Api.js
@@ -1,6 +1,18 @@
 export const apiLogIn = async (email, password) => {
+    let user = {
+      email: email,
+      password: password
+    };
+
     return fetch(
-      `https://loft-taxi.glitch.me/auth?username=${email}&password=${password}`
+      `https://loft-taxi.glitch.me/auth`,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+      }
     ).then(res => res.json()).then(data => data.success);
   };
+  
   
